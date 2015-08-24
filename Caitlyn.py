6e043ddcc79e9f8d47512606080a0934e15c4d2f
csloo <csloo180@gmail.com>
2015-08-23 20:37:31 -0700

    Update Caitlyn.py

--- Caitlyn.py
+++ Caitlyn.py
@@ -19,6 +19,14 @@ ourTurtle.color("yellow")
 ourTurtle.color("purple")
 ourTurtle.color("pink")
 ourTurtle.color("black")
-
+ourTurtle.pencolor("blue")
+ourTurtle.fd(100)
+ourTurtle.lt(90)
+ourTurtle.fd(100)
+ourTurtle.lt(90)
+ourTurtle.fd(100)
+ourTurtle.lt(90)
+ourTurtle.fd(100)
+ourTurtle.
 
 ourScreen.exitonclick()
